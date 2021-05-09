1cfdfffe016e3407ff4c8432d3e9a285bf75f1e1
Dave <bigmanrunning@gmail.com>
2021-05-09 15:36:47 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+<h1>Database First Console Project Generator Script</h1>
+<h3>Using Microsoft EntityFramework Core</h3>
+<hr>
+
+<p>These two bash scrips will work on Linux MicroSoft .NET Core enabled machines.  One could probably build the same for 
+a Windows 10 or Windows Server machine, but that will be up to you all.</p>
+<p>On the command line, you will have to give the name of the database, your user name, and your password in order to 
+create the basic folders, C# .NET Console Program project, and scaffold the database tables (entities) into DbContext classes and 
+Database Model classes.</p>
+<p>However, the Console Program generated is just a basic 'Hello World' program.  You will have to modify the class and add needed 
+classes to develop a database program.  In the script, you can substitute any of the legal ASPnet or .NET projects in place of 'new 
+console', such as 'new web' for a blank web app.</p>
+
+<p>As always, use at your own risk and modify as needed.</p>
---
+++ scaffoldsqlserver.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+if [[ $# -ne 3 ]]; then
+
+    echo "Usage: ./scaffoldmysql name_of_project username passwd"
+    echo "     Where: name_of_project is both project name,"
+    echo "            username is user in mysql database,"
+    echo "            passwd is the user's password in mysql database,"
+    echo "            and name of MySql database name to be scaffolded."
+    exit 2
+fi
+
+echo "Creating folder "$1" for project $1."
+echo ""
+if [[ -d ./$1 ]]; then
+
+    echo "Folder already exists, quitting."
+    exit 1
+fi
+mkdir $1
+sleep 3
+
+echo ""
+echo "Moving to project folder..."
+echo ""
+cd $1
+pwd
+sleep 3
+
+echo "Building base console app..."
+echo ""
+dotnet new console
+echo "Creating Data Context folder..."
+mkdir Data
+echo "Creating Data Models folder..."
+echo ""
+mkdir Models
+sleep 3
+
+echo "Adding EF Core packages plus one for MySQL..."
+echo ""
+echo "First, adding EntityFrameworkCore Tools..."
+echo ""
+dotnet add package Microsoft.EntityframeworkCore.Tools
+wait
+sleep 3
+echo "Second, adding EntityFrameworkCore Design..."
+echo ""
+dotnet add package Microsoft.EntityframeworkCore.Design
+wait
+sleep 3
+echo "Finally, adding SqlServer EntityFrameworkCore Provider/Connector..."
+echo ""
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+wait
+sleep 3
+
+echo "Restore project...."
+echo ""
+dotnet restore
+wait
+sleep 3
+
+echo "Scaffold database into project...."
+echo ""
+
+dotnet ef dbcontext scaffold "Server=localhost;Database="$1";Uid="$2";Pwd="$3";" Microsoft.EntityFrameworkCore.SqlServer --context-dir Data --output-dir Models
+wait
+sleep 3
+
+echo "Restore project again and open VS Code...."
+dotnet restore
+sleep 3
+
+code .
